4463aba16950e4c09d29120c05f22acba48841ce
ShaNaim <shanaim2k15@gmail.com>
2024-11-30 03:58:14 +0600

    :recycle: added utility functions

--- src/utils/provider/queries/user.query.ts
+++ src/utils/provider/queries/user.query.ts
@@ -1,39 +1,41 @@
-import { UserQueryTypes } from '@utils/types';
-type UserQueryOptions = { includePerson: boolean; includeAddress: boolean };
+import { UserQueryTypes, PersonQueryType, personQueryTypes, AddressQueryType, addressQueryOptions, UserQueryOptions } from '@utils/types';
 
-const options: UserQueryOptions = { includePerson: false, includeAddress: false };
-const address = {
+const address = (value: boolean) => ({
     select: {
         slug: true,
-        street: true,
-        city: true,
-        postalCode: true,
-        country: true,
-        district: true,
-        area: true
+        street: value,
+        city: value,
+        postalCode: value,
+        country: value,
+        district: value,
+        area: value
     }
-};
+});
+
+const person = (value: boolean) => ({ firstName: value, lastName: value, phone: value, slug: true });
 
 function addressQuery(includeAddress: boolean) {
-    if (includeAddress) return address;
-    return false;
+    if (includeAddress) return address(true);
+    return address(false);
 }
 
 function personQuery(includePerson: boolean, includeAddress: boolean) {
     if (includePerson)
         return {
             select: {
-                firstName: true,
-                lastName: true,
-                phone: true,
-                slug: true,
+                ...person(true),
                 address: addressQuery(includeAddress)
             }
         };
-    return false;
+    return {
+        select: {
+            ...person(false),
+            address: addressQuery(includeAddress)
+        }
+    };
 }
 
-const generateUserQuery = (options: UserQueryOptions) => ({
+const userQuery = (options: UserQueryOptions) => ({
     select: {
         email: true,
         password: false,
@@ -42,10 +44,35 @@ const generateUserQuery = (options: UserQueryOptions) => ({
     }
 });
 
-export function userQuery(details: UserQueryTypes = null) {
+export function generateUserQuery(details: UserQueryTypes = null) {
+    const options: UserQueryOptions = { includePerson: false, includeAddress: false };
+
     if (details) {
         options.includePerson = true;
         options.includeAddress = true;
     }
-    return generateUserQuery(options);
+    return userQuery(options);
+}
+
+export function generatePersonQuery(details: PersonQueryType) {
+    const options: UserQueryOptions = { includePerson: false, includeAddress: false };
+
+    if (details === personQueryTypes.all) {
+        options.includePerson = true;
+        options.includeAddress = true;
+    }
+    if (details === personQueryTypes.person) {
+        options.includePerson = true;
+        options.includeAddress = false;
+    }
+    if (details === personQueryTypes.address) {
+        options.includePerson = false;
+        options.includeAddress = true;
+    }
+    return personQuery(options.includePerson, options.includeAddress);
+}
+
+export function generateAddressQuery(details: AddressQueryType) {
+    if (details === addressQueryOptions) return addressQuery(true);
+    return addressQuery(false);
 }
--- src/utils/validator/requestSchemaValidator/request.validator.ts
+++ src/utils/validator/requestSchemaValidator/request.validator.ts
@@ -1,5 +1,13 @@
 import { z } from 'zod';
 
+export const detailsSchema = z.object({
+    details: z.string().min(1).optional()
+});
+
+export const detailsQuerySchema = z.object({
+    params: detailsSchema
+});
+
 export const getBySlugSchema = z.object({
     slug: z.string().uuid()
 });
@@ -11,3 +19,6 @@ export const getBySlugParamsSchema = z.object({
 // Type for Registration Data
 export type SlugParamsSchema = z.infer<typeof getBySlugParamsSchema>;
 export type SlugSchema = z.infer<typeof getBySlugSchema>;
+
+export type DetailsQuerySchema = z.infer<typeof detailsQuerySchema>;
+export type DetailsSchema = z.infer<typeof detailsSchema>;
