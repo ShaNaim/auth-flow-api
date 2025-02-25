ba7165ce1b187a6943e8f0e49051eed510ef82e6
ShaNaim <shanaim2k15@gmail.com>
2025-02-25 09:40:50 +0600

    :test_tube: testing log pino-logfmt

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,16 +1,42 @@
-import logger from 'pino';
+import pino from 'pino';
 import dayjs from 'dayjs';
-import server from './config';
 
-const log = logger({
-    transport: {
-        target: 'pino-pretty'
-    },
+const server = {
+    log_level: process.env.LOG_LEVEL || 'info'
+};
+
+const log = pino({
     level: server.log_level,
     base: {
         pid: false
     },
-    timestamp: () => `,"time": "${dayjs().format('MM-DD HH:mm:ss')}"`
+    timestamp: () => `,"time":"${dayjs().format('MM-DD HH:mm:ss')}"`,
+    transport: {
+        targets: [
+            // LogFMT transport for file logging
+            {
+                target: 'pino-logfmt',
+                level: server.log_level,
+                options: {
+                    flattenNestedObjects: true,
+                    convertToSnakeCase: true,
+                    destination: './logs/authflow.log'
+                }
+            },
+            // Pretty transport for console output
+            {
+                target: 'pino-pretty',
+                level: server.log_level,
+                options: {
+                    colorize: true,
+                    translateTime: 'SYS:standard',
+                    ignore: 'pid,hostname',
+                    messageFormat: '{msg} {data}',
+                    singleLine: true
+                }
+            }
+        ]
+    }
 });
 
 export default log;
